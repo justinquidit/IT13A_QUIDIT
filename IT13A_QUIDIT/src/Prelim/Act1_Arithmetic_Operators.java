0e62fdd51ccd0722455469d28c1fee74622b9aab
huntznavarro10 <justinquidit5@gmail.com>
2025-02-14 00:18:19 +0800

    java file
    Added HelloWorld class

--- IT13A_QUIDIT/src/Prelim/Act1_Arithmetic_Operators.java
+++ IT13A_QUIDIT/src/Prelim/Act1_Arithmetic_Operators.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Prelim;
 
-/**
- *
- * @author Administrator
- */
+package Prelim;
+//import java.util.Scanner;
 public class Act1_Arithmetic_Operators {
-    
+    public static void main(String[] org){
+        
+        //Scanner scan = new Scanner(System.in); 
+       //boolean op = true;
+        //while(op){
+        //System.out.print("input firt number = ");
+          //int opop = scan.nextInt();
+          // System.out.print("input second number = ");
+         // int opop1 = scan.nextInt();
+           
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3;
+        
+        //int opopsolt = opop+opop1;
+        
+        int show1 = numb1 * numb3 + numb2;
+        int backup1 = numb1 - numb3;
+        int show2 = backup1 % numb2;
+        int backup2 = numb1 + numb3 + numb2;
+        int show3 = backup2 / numb4;
+        int backup3 = numb3 * numb3;
+        int show4 = numb1 * numb2 - backup3;
+        
+          System.out.println("10 * 4 + 6 ="+show1);
+         System.out.println("(10-4) % 6 = "+show2);
+            System.out.println("(10 + 4 + 6)/3 = "+show3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+show4);
+               //System.out.println("result = "+ opopsolt);
+          
+}
 }
