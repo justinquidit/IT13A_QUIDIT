e61206c43c9e3943c08af2ddbd21bf51f9da9e2c
justinquidit <justinquidit5@gmail.com>
2025-03-09 21:29:03 +0800

    Add files via upload
    palindrome

---
+++ IT13A_QUIDIT/src/Prelim/prelim.java
@@ -0,0 +1,32 @@
+package justin;
+import java.util.Scanner; // Import Scanner class
+
+public class prelim {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+          System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+       
+
+        // Remove spaces and convert to lowercase
+        String cleaned = original.replaceAll("\\s+", "").toLowerCase();
+
+        // Reverse the string
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        // Check if palindrome
+        if (cleaned.equals(reversed)) {
+            System.out.println("The string is a palindrome!");
+            System.out.println(original);
+            System.out.println(cleaned);
+              System.out.println(reversed);
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+            System.out.println(original);
+            System.out.println(cleaned);
+              System.out.println(reversed);
+            
+        }
+    }
+}
